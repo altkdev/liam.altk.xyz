3cda3d534c3cad2934bc2f50e7ac962a01cb3fc6
Marcus Key <114118968+imagic-eagle@users.noreply.github.com>
2022-10-15 12:33:51 -0600

    Create script.js
    Signed-off-by: Marcus Key <114118968+imagic-eagle@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,17 @@
+var no = a
+alert("bingleton hornboggler III");
+alert ("qjkdlsqdfskjgqjkdfsgkjqdfsjkgsdjkfgjksdqfjkgsdfjkqgkjdsfkjgskjdfgkqjjkgfkjgjldfqgkfqqqqqqqqqq");
+function redirectFunction(){
+  window.location.replace("https://www.tiktok.com/@vexbolts/video/7154490252063853870?is_copy_url=1&is_from_webapp=v1")
+}
+function theFunction(){
+  window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+}
+function myFunction(){
+  alert("go home bozo")
+}
+function crash() {
+  while(1) {
+    no += aa
+  }
+}
